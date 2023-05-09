27e70025101a08bf556c51e268a2463c802a76cb
sallymills00 <122421362+sallymills00@users.noreply.github.com>
2023-05-09 23:11:20 +0900

    Update timetable.py

--- timetable.py
+++ timetable.py
@@ -1,96 +1,91 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-from flask import Flask, request
-from flask_restful import Resource, Api
 
-app=Flask(__name__)
-api=Api(app)
+data = {
+    "Mon":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "Tue":[1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1],
+    "Wed":[0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1],
+    "Thu":[1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0,1,1,1,1,1,0,0,0,0,0],
+    "Fri":[1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0,1,0],
+    "Sat":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0],
+    "Sun":[0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+}
 
-class Result(Resource):
+df = pd.DataFrame(data)
 
-    def post(self, teamID):
-        req_data = request.get_json()
-        resultImageUrl = req_data["resultImageUrl"]
-        resultImage = req_data["time"]
+timeSegment = ["9:00",
+               "9:30",
+               "10:00",
+               "10:30",
+               "11:00",
+               "11:30",
+               "12:00",
+               "12:30",
+               "13:00",
+               "13:30",
+               "14:00",
+               "14:30",
+               "15:00",
+               "15:30",
+               "16:00",
+               "16:30",
+               "17:00",
+               "17:30",
+               "18:00",
+               "18:30",
+               "19:00",
+               "19:30",
+               "20:00",
+               "20:30",
+               "21:00",
+               "21:30",
+               "22:00",
+               "22:30",
+               "23:00",
+               "23:30"]
 
-        #when importing json data use pd_object = pd.read_json(whatever)
+fig, ax = plt.subplots(figsize=(7,10))
+plt.subplots_adjust(left=0.25)
+plt.rcParams['text.color']='black'
 
-        df = pd.DataFrame(resultImage)
+timetable = plt.table(cellText=df.values,
+                      cellLoc='center',
+                      colLabels=list(data.keys()),
+                      rowLabels=timeSegment[0:len(df.values)],
+                      rowLoc='center',
+                      loc='center',
+                      bbox=(0,0,1,1))
+ax.axis(xmax=1.4, ymax=31)
+ax.axis('off')
 
-        timeSegment = ["9:00~9:30",
-               "9:30~10:00",
-               "10:00~10:30",
-               "10:30~11:00",
-               "11:00~11:30",
-               "11:30~12:00",
-               "12:00~12:30",
-               "12:30~13:00",
-               "13:00~13:30",
-               "13:30~14:00",
-               "14:00~14:30",
-               "14:30~15:00",
-               "15:00~15:30",
-               "15:30~16:00",
-               "16:00~16:30",
-               "16:30~17:00",
-               "17:00~17:30",
-               "17:30~18:00",
-               "18:00~18:30",
-               "18:30~19:00",
-               "19:00~19:30",
-               "19:30~20:00",
-               "20:00~20:30",
-               "20:30~21:00",
-               "21:00~21:30",
-               "21:30~22:00",
-               "22:00~22:30",
-               "22:30~23:00",
-               "23:00~23:30",
-               "23:30~24:00"]
+#data cells
+for i in range(1,len(df.values)+1):
+    for j in range(0,len(data.keys())):
+        cell = timetable[i,j]
+        cell.set_height(0.1)
+        cell.get_text().set_visible(False)
+        cell_value = cell.get_text().get_text()
+        cell.set_edgecolor('black')
+        if cell_value == "0":
+            cell.set_facecolor('orange')
+        elif cell_value == "1":
+            cell.set_facecolor('white')
 
-        fig, ax = plt.subplots()
-
-        timetable = ax.table(cellText=df.values, cellLoc='center', colLabels=list(resultImage.keys()), rowLabels=timeSegment[0:len(df.values)], rowLoc='center', loc='center')
-        ax.axis('off')
-
-        #change color based on value
-        for i in range(0,len(df.values)+1):
-            for j in range(0,len(resultImage.keys())):
-                cell = timetable[i,j]
-                cell_value = cell.get_text().get_text()
-                if cell_value == "0":
-                    cell.set_facecolor('orange')
-                elif cell_value == "1":
-                    cell.set_facecolor('gray')
-
-        #change height of rows
-        for i in range(0, len(resultImage.keys())):
-            for j in range(1, len(df.values)+1):
-                cell = timetable[j,i]
-                cell.set_height(0.2)
-            
-        #change height of row headers
-        for i in range(-1, len(resultImage.keys())):
-            for j in range(1, len(df.values)+1):
-                cell = timetable[j,i]
-                cell.set_height(0.2)
-
-        #hide text
-        for i in range(0, len(resultImage.keys())):
-            for j in range(1, len(df.values)+1):
-                cell = timetable[j,i]
-                cell.get_text().set_visible(False)
-
-        #row header boundary
-        for j in range(1, len(df.values)+1):
-            cell = timetable[j,-1]
-            cell.set_edgecolor('black')
+#col headers
+for i in range(0, len(data.keys())):
+        cell = timetable[0,i]
+        cell.set_height(0.1)
+        cell.set_edgecolor('black')
+        cell.visible_edges = 'vertical'
         
-        plt.savefig('tttrial.png')
-        return resultImage
+#row headers
+for j in range(1, len(df.values)+1):
+    cell = timetable[j,-1]
+    cell.set_height(0.1)
+    cell.set_edgecolor('black')
+    cell.visible_edges = 'horizontal'
 
-api.add_resource(Result,'/teams/<int:teamID>/result')
 
-if __name__ == '__main__':
-    app.run(debug=True)
+plt.savefig("test_table.jpg",bbox_inches='tight')
+plt.show()
